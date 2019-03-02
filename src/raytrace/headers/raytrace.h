cb5c98d55e43f539bdedd6f368f993f3bd642c8f
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-02 17:26:38 -0500

    Add raytrace header

--- src/raytrace/headers/raytrace.h
+++ src/raytrace/headers/raytrace.h
@@ -1,7 +1,11 @@
 #ifndef _RAYTERM_RAYTRACER_H_
 #define _RAYTERM_RAYTRACER_H_
 
+#include "renderer.h"
+
 // parameters are (complete, total)
 using progress_callback = void (*)(int, int);
+
+int write_buffer(const char* filename, PixelBuffer* buffer, progress_callback cb = nullptr);
 
 #endif
