6d16c96e7ab39910abcee0a0e59aa255a7b212dd
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-02 18:28:11 -0500

    Rewrite ppm_test function to differentiate writing and rendering

--- src/raytrace/test/ppm_tests.cpp
+++ src/raytrace/test/ppm_tests.cpp
@@ -8,25 +8,21 @@
 #define RES_MULT 1.0
 #endif
 
-void progress_cb(int current, int total) {
-#ifndef NO_PROGRESS
-    printf("\b\b\b%02d%%", int(current * 100.0 / total));
-#endif
-}
-
 TEST(RaytracerTest, Success) {
+    std::chrono::steady_clock::time_point begin, end;
     auto renderer = new Renderer(int(80 * RES_MULT), int(52 * RES_MULT));
 
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-
+    begin = std::chrono::steady_clock::now();
     renderer->launch();
-
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    end = std::chrono::steady_clock::now();
 
     printf("Rendered in %ldms\n",
         std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count());
 
-    printf("Writing Image...\n00%%");
-    write_buffer("test_image.ppm", renderer->buffer(), progress_cb);
-    printf("\b\b\b100%% -- Render Complete!\n");
+    begin = std::chrono::steady_clock::now();
+    write_buffer("test_image.ppm", renderer->buffer());
+    end = std::chrono::steady_clock::now();
+
+    printf("Written in %ldms\n",
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count());
 }
