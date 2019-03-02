c9b338cecd8e732470ba2d85de5840a240d44631
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-01 22:52:08 -0500

    Refactor terminal.h and terminal.cpp to both use a class and follow guides

--- src/rayterm/cpp/terminal.cpp
+++ src/rayterm/cpp/terminal.cpp
@@ -1,8 +1,8 @@
 #include "terminal.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 
-TERMINAL* setup() {
+Terminal::Terminal() {
     initscr();
     start_color();
     cbreak();
@@ -10,27 +10,37 @@ TERMINAL* setup() {
 
     curs_set(0);
 
+    init_pair(1, COLOR_WHITE, COLOR_BLUE);
+    init_pair(2, COLOR_WHITE, COLOR_GREEN);
 
-    init_pair(1,COLOR_WHITE, COLOR_BLUE);
-    init_pair(2,COLOR_WHITE, COLOR_GREEN);
+    main = subwin(stdscr, LINES - 1, 0, 0, 0);
+    info = subwin(stdscr, 1, 0, LINES - 1, 0);
+    wbkgd(main, COLOR_PAIR(2));
+    wbkgd(info, COLOR_PAIR(1));
 
-    TERMINAL* term = (TERMINAL*) malloc(sizeof(TERMINAL));
-    term->main = subwin(stdscr, LINES-1, 0, 0, 0);
-    term->info = subwin(stdscr, 1, 0, LINES-1, 0);
-    wbkgd(term->main, COLOR_PAIR(2));
-    wbkgd(term->info, COLOR_PAIR(1));
-
-    keypad(term->main, true);
-    keypad(term->info, true);
-
-    return term;
+    keypad(main, true);
+    keypad(info, true);
 }
 
-void repaint(TERMINAL* term) {
-
+// Close down Terminal
+Terminal::~Terminal() {
+    delwin(main);
+    delwin(info);
+    endwin();
 }
 
-void handle_resize(TERMINAL* term) {
+void Terminal::render() {
+    // fill buffer with raytrace
+    // convert buffer to unicode
+
+    // refresh terminal
+    wrefresh(main);
+    // wrefresh(info);
+}
+
+void Terminal::handle_resize() {
+    getmaxyx(stdscr, width, height);
+    printf("Resized to %dx%d (%d COLS, %d LINES)", width, height, COLS, LINES);
     /*wresize(stdscr, LINES, COLS);
     wclear(stdscr);
     wresize(WINDOWS.info, 1, COLS);
@@ -46,41 +56,16 @@ void handle_resize(TERMINAL* term) {
     */
 }
 
-
-VECTOR* get_size(WINDOW* win) {
-    VECTOR* sizes = (VECTOR*) malloc(sizeof(VECTOR));
-    getmaxyx(win, sizes->y, sizes->x);
-
-    return sizes;
+// auto-refreshes info window
+void Terminal::set_info_string(const char* str) {
+    wclear(info);
+    wmove(info, 0, 0);
+    add_str(info, str, A_BOLD);
+    wrefresh(info);
 }
 
 void add_str(WINDOW* window, const char* str, chtype attr) {
-    for(int i = 0; str[i] != '\0'; i++) {
+    for (int i = 0; str[i] != '\0'; i++) {
         waddch(window, str[i] | attr);
     }
-}
-
-// auto-refreshes info window
-void set_info_string(TERMINAL* term, const char* str) {
-    wclear(term->info);
-    wmove(term->info, 0, 0);
-    add_str(term->info, str, A_BOLD);
-    wrefresh(term->info);
-}
-
-// must call refresh after
-void set_character_pixel(TERMINAL* term, POINT* pos, const char ch, const Color background, const Color foreground) {
-    wmove(term->main, pos->x, pos->y);
-    chtype pixel = ch;
-    // TODO: add background and foreground to pixel
-    waddch(term->main, pixel);
-}
-
-// Close down Terminal
-void cleanup(TERMINAL* term) {
-    delwin(term->main);
-    delwin(term->info);
-    endwin();
-    free(term);
-    printf("\n");
 }
