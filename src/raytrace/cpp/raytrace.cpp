1f62afc2672212b6de3a5e578e9b62cc4c4c554a
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-02 17:27:39 -0500

    Update write_buffer to write out the values in the buffer
    This may still be writing the values upside down

--- src/raytrace/cpp/raytrace.cpp
+++ src/raytrace/cpp/raytrace.cpp
@@ -1,5 +1,8 @@
 #include "raytrace.h"
+#include <optix.h>
+#include <optix_world.h>
 #include <cstdio>
+#include <fstream>
 
 #ifndef SAMPLES
 #define SAMPLES 32
@@ -9,33 +12,39 @@
 #define RES_MULT 1.0
 #endif
 
-int launch(progress_callback cb /* = nullptr */) {
-    FILE* outfile = fopen("test_image.ppm", "w");
-    if (outfile == nullptr) {
-        fprintf(stderr, "Error opening %s!\n", "test_image.ppm");
+int write_buffer(const char* filename, PixelBuffer* buffer, progress_callback cb /* = nullptr */) {
+    std::fstream outfile(filename, std::fstream::out | std::fstream::binary | std::fstream::trunc);
+    if (outfile.fail()) {
+        fprintf(stderr, "Error opening %s!\n", filename);
         return 1;
     }
 
     auto width  = int(80 * RES_MULT);
     auto height = int(52 * RES_MULT);
 
-    fprintf(outfile, "P3\n%i %i\n255\n", width, height);
+    outfile << "P3 ";
+    outfile << width << " " << height << " " << 255 << " ";
     int pixels = width * height;
     int pixel  = 0;
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            for (int sample = 0; sample < SAMPLES; sample++) {
-            }
-
             // gamma correct outcol
             // raise to 1 / gamma (1/2 in our case)
 
-            fprintf(outfile, "%i %i %i\n", 0, 0, 0);
+            optix::float4 sample = buffer->get(x, y);
+
+            // print red
+            outfile << sample.x << " ";
+            // print green
+            outfile << sample.y << " ";
+            // print blue
+            outfile << sample.z << " ";
             if (cb != nullptr) {
                 cb(pixel++, pixels);
             }
         }
     }
-    fclose(outfile);
+    outfile.close();
+    delete buffer;
     return 0;
 }
