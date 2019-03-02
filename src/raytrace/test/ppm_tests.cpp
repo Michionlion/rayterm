0b006f4a9cb81bff758396f4683849fbb2b1ee66
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-02 00:58:52 -0500

    Update ppm_tests to run the new launch Renderer function

--- src/raytrace/test/ppm_tests.cpp
+++ src/raytrace/test/ppm_tests.cpp
@@ -1,7 +1,11 @@
 #include <gtest/gtest.h>
 #include <chrono>
 #include <cstdio>
-#include "raytrace.h"
+#include "renderer.h"
+
+#ifndef RES_MULT
+#define RES_MULT 1.0
+#endif
 
 void progress_cb(float progress) {
 #ifndef NO_PROGRESS
@@ -10,17 +14,17 @@ void progress_cb(float progress) {
 }
 
 TEST(RaytracerTest, Success) {
-    printf("00%%");
+    auto renderer = new Renderer(int(80 * RES_MULT), int(52 * RES_MULT));
+
+    // printf("00%%");
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
-    int status = launch(progress_cb);
+    renderer->launch();
 
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
-    printf("\b\b\b100%% -- Render Complete!\n");
+    // printf("\b\b\b100%% -- Render Complete!\n");
 
     printf("Completed in %ldms\n",
         std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count());
-
-    EXPECT_EQ(status, 0) << "Did not successfully generate a test image!";
 }
