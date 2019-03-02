7b8a93db4860d35e54fd99c705763f4a55873925
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-02 00:58:18 -0500

    Remove unneeded contents in raytrace

--- src/raytrace/cpp/raytrace.cpp
+++ src/raytrace/cpp/raytrace.cpp
@@ -20,8 +20,8 @@ int launch(progress_callback cb /* = nullptr */) {
     int height = int(52 * RES_MULT);
 
     fprintf(outfile, "P3\n%i %i\n255\n", width, height);
-    float pixels = width * height;
-    int pixel    = 0;
+    int pixels = width * height;
+    int pixel  = 0;
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             for (int sample = 0; sample < SAMPLES; sample++) {
@@ -32,7 +32,7 @@ int launch(progress_callback cb /* = nullptr */) {
 
             fprintf(outfile, "%i %i %i\n", 0, 0, 0);
             if (cb != nullptr) {
-                cb(pixel++ / pixels);
+                cb(pixel++, pixels);
             }
         }
     }
--- src/raytrace/headers/raytrace.h
+++ src/raytrace/headers/raytrace.h
@@ -1,8 +1,7 @@
 #ifndef _RAYTERM_RAYTRACER_H_
 #define _RAYTERM_RAYTRACER_H_
 
-using progress_callback = void (*)(float);
-
-int launch(progress_callback cb = nullptr);
+// parameters are (complete, total)
+using progress_callback = void (*)(int, int);
 
 #endif
