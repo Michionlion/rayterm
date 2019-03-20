3253166634cc960a11daf47c77164862839f5ce4
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-20 15:56:58 -0400

    Make Mesh attribs public

--- src/raytrace/headers/geometry.h
+++ src/raytrace/headers/geometry.h
@@ -3,22 +3,24 @@
 
 #include <optix.h>
 #include <optix_world.h>
+#include <cstdlib>
 #include "tiny_obj_loader.h"
 
-class ObjFile {
-    tinyobj::attrib_t attrib;
-    std::vector<tinyobj::shape_t> shapes;
-    std::vector<tinyobj::material_t> materials;
+// Code here is roughly inspired from OptiXMesh in the OptiX samples
+// https://github.com/nvpro-samples/optix_advanced_samples
 
-    ObjFile(tinyobj::attrib_t attrib, std::vector<tinyobj::shape_t> shapes,
+class Mesh {
+   public:
+    Mesh(tinyobj::attrib_t attrib, std::vector<tinyobj::shape_t> shapes,
         std::vector<tinyobj::material_t> materials) {
         this->attrib    = std::move(attrib);
         this->shapes    = std::move(shapes);
         this->materials = std::move(materials);
     };
 
-   public:
-    optix::Geometry getOptixGeometry();
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
 };
 
 ObjFile* loadObj(const char* objfile);
