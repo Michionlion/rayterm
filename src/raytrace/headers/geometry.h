dddfe6150334535a3d7b291c10fea1544c41aefd
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-20 15:57:10 -0400

    Add MeshResources class

--- src/raytrace/headers/geometry.h
+++ src/raytrace/headers/geometry.h
@@ -23,6 +23,35 @@ class Mesh {
     std::vector<tinyobj::material_t> materials;
 };
 
-ObjFile* loadObj(const char* objfile);
+class MeshResources {
+    Mesh** meshes = nullptr;
+    int size      = 0;
 
+   public:
+    MeshResources(){};
+    ~MeshResources() {
+        for (int i = 0; i < size; i++) {
+            delete meshes[i];
+        }
+
+        free(meshes);
+    };
+
+    Mesh* getMesh(int id) { return meshes[id]; }
+
+    // pass in a mesh and MeshResources will handle the rest
+    // returns the mesh id
+    int addMesh(Mesh* mesh) {
+        meshes       = (Mesh**)realloc(meshes, (size + 1) * sizeof(Mesh*));
+        meshes[size] = mesh;
+        return size++;
+    }
+
+    // load obj file and return mesh id
+    // this method allocates a new Mesh
+    // and adds it to the meshes array
+    int loadObjFile(const char* objfile);
+
+    optix::GeometryInstance createGeometryInstance(const int mesh_id);
+};
 #endif
