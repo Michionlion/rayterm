fcc9ca7fb1bf44eeca31537ba21e7d6c7498a791
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-21 01:07:52 -0400

    Remove display method

--- src/raytrace/shaders/raygen.cu
+++ src/raytrace/shaders/raygen.cu
@@ -28,46 +28,6 @@ rtDeclareVariable(float3, sysCameraU, , );
 rtDeclareVariable(float3, sysCameraV, , );
 rtDeclareVariable(float3, sysCameraW, , );
 
-// exposure + simple Reinhard tonemapper + gamma
-RT_FUNCTION uchar4 display(float3 val) {
-    // clamp
-    // val.x = fminf(fmaxf(val.x, 0.0f), 1.0f);
-    // val.y = fminf(fmaxf(val.y, 0.0f), 1.0f);
-    // val.z = fminf(fmaxf(val.z, 0.0f), 1.0f);
-
-    // do tonemap
-    // val *= 1.0f;
-    // const float burn_out = 0.1f;
-    // val.x *= (1.0f + val.x * burn_out) / (1.0f + val.x);
-    // val.y *= (1.0f + val.y * burn_out) / (1.0f + val.y);
-    // val.z *= (1.0f + val.z * burn_out) / (1.0f + val.z);
-    // // these WERE in z->y->x order
-    // return make_uchar4(
-    //     (unsigned char)(255.0 * fminf(powf(fmaxf(val.x, 0.0f), (float)(1.0 / 2.2)), 1.0f)),
-    //     (unsigned char)(255.0 * fminf(powf(fmaxf(val.y, 0.0f), (float)(1.0 / 2.2)), 1.0f)),
-    //     (unsigned char)(255.0 * fminf(powf(fmaxf(val.z, 0.0f), (float)(1.0 / 2.2)), 1.0f)), 255);
-
-    // do bt_709
-    uchar4 output = make_uchar4(0u, 255u, 0u, 255u);
-    if (val.x < 0.018) {
-        output.x = (unsigned char)(val.x * 4.5 * 255.0);
-    } else {
-        output.x = (unsigned char)(255.0 * (1.099 * pow(val.x, 0.45) - 0.099));
-    }
-    if (val.y < 0.018) {
-        output.y = (unsigned char)(val.y * 4.5 * 255.0);
-    } else {
-        output.y = (unsigned char)(255.0 * (1.099 * pow(val.y, 0.45) - 0.099));
-    }
-    if (val.z < 0.018) {
-        output.z = (unsigned char)(val.z * 4.5 * 255.0);
-    } else {
-        output.z = (unsigned char)(255.0 * (1.099 * pow(val.z, 0.45) - 0.099));
-    }
-
-    return output;
-}
-
 // Entry point
 RT_PROGRAM void raygen() {
     RayPayload payload;
@@ -96,5 +56,22 @@ RT_PROGRAM void raygen() {
     // start the ray traversal at the root object
     rtTrace(sysRootObject, ray, payload);
 
-    sysOutputBuffer[theLaunchIndex] = display(payload.radiance);
+    uchar4 output = make_uchar4(0u, 255u, 0u, 255u);
+    if (payload.radiance.x < 0.018) {
+        output.x = (unsigned char)(payload.radiance.x * 4.5 * 255.0);
+    } else {
+        output.x = (unsigned char)(255.0 * (1.099 * pow(payload.radiance.x, 0.45) - 0.099));
+    }
+    if (payload.radiance.y < 0.018) {
+        output.y = (unsigned char)(payload.radiance.y * 4.5 * 255.0);
+    } else {
+        output.y = (unsigned char)(255.0 * (1.099 * pow(payload.radiance.y, 0.45) - 0.099));
+    }
+    if (payload.radiance.z < 0.018) {
+        output.z = (unsigned char)(payload.radiance.z * 4.5 * 255.0);
+    } else {
+        output.z = (unsigned char)(255.0 * (1.099 * pow(payload.radiance.z, 0.45) - 0.099));
+    }
+
+    sysOutputBuffer[theLaunchIndex] = output;
 }
