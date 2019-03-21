ff2de58dbf856a896f81eb0dba5b9f6e2e708645
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-21 00:16:35 -0400

    Add diffuse skeleton shader

---
+++ src/raytrace/shaders/diffuse.cu
@@ -0,0 +1,11 @@
+#include "config.h"
+
+#include <optix.h>
+#include <optix_world.h>
+#include <optixu/optixu_math_namespace.h>
+
+#include "payload.h"
+
+RT_PROGRAM void closest_hit() {}
+
+RT_PROGRAM void any_hit() {}
