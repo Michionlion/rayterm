c5578decf67ddf5118ee9c3a6bfaf2c24a4f9d47
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-20 23:07:47 -0400

    Add material support to Resources header

--- src/raytrace/headers/geometry.h
+++ src/raytrace/headers/geometry.h
@@ -30,6 +30,8 @@ class Resources {
     Mesh** meshes   = nullptr;
     int meshes_size = 0;
 
+    std::vector<optix::Material> materials;
+
    public:
     explicit Resources(const optix::Context& context) : context(context){};
     ~Resources() {
@@ -39,6 +41,10 @@ class Resources {
             }
 
             free(meshes);
+
+            for (auto material : materials) {
+                material->destroy();
+            }
         } catch (const optix::Exception& ex) {
             printf("~Resources Error: %d (%s)\n", ex.getErrorCode(), ex.getErrorString().c_str());
         }
@@ -54,12 +60,24 @@ class Resources {
         return meshes_size++;
     }
 
+    optix::Material getMaterial(int id) { return materials[id]; }
+    int addMaterial(const optix::Material& mat) {
+        materials.push_back(mat);
+        return materials.size() - 1;
+    }
+
     // load obj file and return mesh id
     // this method allocates a new Mesh
     // and adds it to the meshes array
     int loadObjFile(std::string objfile);
     int loadObjFile(const char* objfile) { return loadObjFile(std::string(objfile)); }
 
-    optix::GeometryInstance createGeometryInstance(const int mesh_id);
+    // load mat file and return mat id
+    // this method creates a material
+    // and adds it to the materials vector
+    int loadMatFile(std::string matfile);
+    int loadMatFile(const char* matfile) { return loadMatFile(std::string(matfile)); }
+
+    optix::GeometryInstance createGeometryInstance(int mesh_id, int mat_id);
 };
 #endif
