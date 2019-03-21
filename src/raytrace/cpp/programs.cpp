af35541357652b3613b88a9253c920d4305f4f26
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-21 00:19:31 -0400

    Fix many small problems in programs implementation, type naming, etc.

--- src/raytrace/cpp/programs.cpp
+++ src/raytrace/cpp/programs.cpp
@@ -7,35 +7,38 @@
 #include <map>
 #include <string>
 
-bool Programs::load(const char* file, const char* func, const char* id) {
+bool Programs::load(const char* file, const char* func, const std::string& id) {
     std::string path = std::string(PROGRAM_DIRECTORY) + "/" + file + ".ptx";
     try {
-        programs[id] = context->createProgramFromPTXFile(path, func);
+        program_map[id] = context->createProgramFromPTXFile(path, func);
         printf("Compiled '%s' to '%s'\n", path.c_str(), id.c_str());
-        programs[id]->validate();
+        program_map[id]->validate();
     } catch (optix::Exception& ex) {
-        printf("Error compiling '%s': %d (%s)\n", name.c_str(), ex.getErrorCode(),
+        printf("Error compiling '%s': %d (%s)\n", id.c_str(), ex.getErrorCode(),
             ex.getErrorString().c_str());
+        return false;
     }
+
+    return true;
 }
 
 optix::Program Programs::get(const char* file, const char* func) {
-    std::string id                = std::string(file) + ":" + func;
-    ProgramMap::const_iterator it = programs.find(id);
-    if (it == programs.end()) {
-        bool loaded = load(file, func);
+    const std::string id                                     = std::string(file) + ":" + func;
+    std::map<std::string, optix::Program>::const_iterator it = program_map.find(id);
+    if (it == program_map.end()) {
+        bool loaded = load(file, func, id);
         if (!loaded) {
-            throw std::runtime_error(sprintf("Error: could not find %s", id));
+            throw std::runtime_error("Error: could not find " + id);
         }
-        it = programs.find(name);
+        it = program_map.find(id);
     }
     return it->second;
 }
 
-optix::Program Programs::get(const char* id) {
-    ProgramMap::const_iterator it = programs.find(id);
-    if (it == programs.end()) {
-        throw std::runtime_error(sprintf("Error: could not find required ptx program %s", id));
+optix::Program Programs::get(const std::string& id) {
+    std::map<std::string, optix::Program>::const_iterator it = program_map.find(id);
+    if (it == program_map.end()) {
+        throw std::runtime_error(std::string("Error: could not find required ptx program ") + id);
     }
     return it->second;
 }
