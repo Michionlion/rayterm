bfcec779e1b4e80eae3ae806a59db0cdfbbd65aa
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-22 19:13:05 -0400

    Add exception catching for rtime

--- src/rtime/cpp/rtime.cpp
+++ src/rtime/cpp/rtime.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <cstdio>
 #include <cstring>
+#include <exception>
 #include "rayterm"
 #include "raytrace.h"
 #include "renderer.h"
@@ -10,46 +11,50 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "Usage: rtime <N> <RES_MULT> <SAMPLES>\n");
         exit(1);
     }
+    try {
+        int n          = std::stoi(argv[1]);
+        float res_mult = std::stof(argv[2]);
+        int samples    = std::stoi(argv[3]);
+        int width      = int(80 * res_mult);
+        int height     = int(52 * res_mult);
+        printDeviceInfo();
+        printf("Rendering %dx%d, %dspp images\n", width, height, samples);
 
-    int n          = std::stoi(argv[1]);
-    float res_mult = std::stof(argv[2]);
-    int samples    = std::stoi(argv[3]);
-    int width      = int(80 * res_mult);
-    int height     = int(52 * res_mult);
-    printDeviceInfo();
-    printf("Rendering %dx%d, %dspp image\n", width, height, samples);
+        auto renderer = new Renderer(width, height, samples);
+        std::chrono::steady_clock::time_point begin, end;
+        int64_t times[n];
 
-    auto renderer = new Renderer(width, height, samples);
-    std::chrono::steady_clock::time_point begin, end;
-    long times[n];
+        for (int i = 0; i < n; i++) {
+            delete renderer;
+            renderer = new Renderer(width, height, samples);
 
-    for (int i = 0; i < n; i++) {
-        delete renderer;
-        renderer = new Renderer(width, height, samples);
+            printf("Start run %d\n", i + 1);
+            begin = std::chrono::steady_clock::now();
+            renderer->launch();
+            end = std::chrono::steady_clock::now();
+            printf("End run %d, ", i + 1);
 
-        printf("Start run %d\n", i + 1);
+            times[i] = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+            printf("time: %ldms\n", times[i]);
+        }
+
+        int64_t average_time = 0;
+        for (int i = 0; i < n; i++) {
+            average_time += times[i];
+        }
+        average_time /= n;
+
+        printf("\nSUMMARY\n\nRendered %dx%d, %dspp images in %ldms on average\n", width, height,
+            samples, average_time);
+
         begin = std::chrono::steady_clock::now();
-        renderer->launch();
+        write_buffer("test_image.ppm", renderer->buffer());
         end = std::chrono::steady_clock::now();
-        printf("End run %d, ", i + 1);
 
-        times[i] = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-        printf("time: %ldms\n", times[i]);
+        printf("\nWritten in %ldms\n\n-------\n",
+            std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count());
+    } catch (const std::exception& ex) {
+        printf("Exception in rtime: (%s)\n", ex.what());
+        exit(1);
     }
-
-    long average_time = 0;
-    for (int i = 0; i < n; i++) {
-        average_time += times[i];
-    }
-    average_time /= n;
-
-    printf("\nSUMMARY\n\nRendered %dx%d, %dspp images in %ldms on average\n", width, height,
-        samples, average_time);
-
-    begin = std::chrono::steady_clock::now();
-    write_buffer("test_image.ppm", renderer->buffer());
-    end = std::chrono::steady_clock::now();
-
-    printf("\nWritten in %ldms\n\n-------\n",
-        std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count());
 }
