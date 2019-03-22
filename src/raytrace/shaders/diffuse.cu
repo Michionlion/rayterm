645c5021140b3023a962248a8a0866a12d0b8706
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-22 02:36:18 -0400

    Draw real shading normal, normalize shading normal, and remove extras

--- src/raytrace/shaders/diffuse.cu
+++ src/raytrace/shaders/diffuse.cu
@@ -25,25 +25,24 @@ rtDeclareVariable(RayPayload, thePayload, rtPayload, );
 rtDeclareVariable(optix::float3, varColor, varColor, );
 
 RT_PROGRAM void closest_hit() {
-    const optix::int3 v_idx       = vidx[rtGetPrimitiveIndex()];
-    const optix::float3 v0        = vbuf[v_idx.x];
-    const optix::float3 v1        = vbuf[v_idx.y];
-    const optix::float3 v2        = vbuf[v_idx.z];
-    const optix::float3 Ng        = optix::cross(v1 - v0, v2 - v0);
-    const float3 geometric_normal = optix::normalize(Ng);
+    const optix::int3 v_idx = vidx[rtGetPrimitiveIndex()];
+    const optix::float3 v0  = vbuf[v_idx.x];
+    const optix::float3 v1  = vbuf[v_idx.y];
+    const optix::float3 v2  = vbuf[v_idx.z];
+    // const optix::float3 Ng        = optix::cross(v1 - v0, v2 - v0);
+    // const float3 geometric_normal = optix::normalize(Ng);
 
-    const optix::int3 n_idx            = nidx[rtGetPrimitiveIndex()];
-    const optix::float3 shading_normal = nbuf[n_idx.y] * barycentrics.x +
-                                         nbuf[n_idx.z] * barycentrics.y +
-                                         nbuf[n_idx.x] * (1.0f - barycentrics.x - barycentrics.y);
+    const optix::int3 n_idx = nidx[rtGetPrimitiveIndex()];
+    const optix::float3 shading_normal =
+        optix::normalize(nbuf[n_idx.y] * barycentrics.x + nbuf[n_idx.z] * barycentrics.y +
+                         nbuf[n_idx.x] * (1.0f - barycentrics.x - barycentrics.y));
 
     // optix::float3 dir = random_in_uhemisphere(thePayload.rand, geometric_normal);
     // thePayload.rand   = dir.z;
 
     // thePayload.radiance =
     //     make_float3(barycentrics.x, barycentrics.y, 1.0f - barycentrics.x - barycentrics.y);
-    thePayload.radiance =
-        optix::normalize(rtTransformNormal(RT_OBJECT_TO_WORLD, shading_normal)) * 0.5f + 0.5f;
+    thePayload.radiance = shading_normal * 0.5f + 0.5f;
 }
 
 RT_PROGRAM void any_hit() {}
