78646b7c136d699a5800c820d7ac3f0a1a9a16c1
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-22 03:45:41 -0400

    Implement recursive lambert diffuse material based on cpu version

--- src/raytrace/shaders/diffuse.cu
+++ src/raytrace/shaders/diffuse.cu
@@ -10,8 +10,8 @@
 // very slightly inspired from OptiX SDK example 'optixGeometryTriangles'
 // (there's literally no documentation on GeometryTriangles)
 
+rtDeclareVariable(rtObject, sysRootObject, , );
 rtDeclareVariable(float2, barycentrics, attribute rtTriangleBarycentrics, );
-
 rtDeclareVariable(float, distance, rtIntersectionDistance, );
 
 rtBuffer<float3> vbuf;
@@ -22,27 +22,34 @@ rtBuffer<int3> nidx;
 rtDeclareVariable(optix::Ray, theRay, rtCurrentRay, );
 rtDeclareVariable(RayPayload, thePayload, rtPayload, );
 
-rtDeclareVariable(optix::float3, varColor, varColor, );
+rtDeclareVariable(optix::float3, varAttenuation, , );
 
 RT_PROGRAM void closest_hit() {
-    const optix::int3 v_idx = vidx[rtGetPrimitiveIndex()];
-    const optix::float3 v0  = vbuf[v_idx.x];
-    const optix::float3 v1  = vbuf[v_idx.y];
-    const optix::float3 v2  = vbuf[v_idx.z];
-    // const optix::float3 Ng        = optix::cross(v1 - v0, v2 - v0);
-    // const float3 geometric_normal = optix::normalize(Ng);
+    thePayload.depth -= 1;
+    if (thePayload.depth > 0) {
+        const optix::int3 v_idx = vidx[rtGetPrimitiveIndex()];
+        const optix::float3 v0  = vbuf[v_idx.x];
+        const optix::float3 v1  = vbuf[v_idx.y];
+        const optix::float3 v2  = vbuf[v_idx.z];
 
-    const optix::int3 n_idx = nidx[rtGetPrimitiveIndex()];
-    const optix::float3 shading_normal =
-        optix::normalize(nbuf[n_idx.y] * barycentrics.x + nbuf[n_idx.z] * barycentrics.y +
-                         nbuf[n_idx.x] * (1.0f - barycentrics.x - barycentrics.y));
+        // calculate geometric (face) normal
+        // const optix::float3 Ng        = optix::cross(v1 - v0, v2 - v0);
+        // const float3 normal = optix::normalize(Ng);
 
-    // optix::float3 dir = random_in_uhemisphere(thePayload.rand, geometric_normal);
-    // thePayload.rand   = dir.z;
+        // calculate interpolated vertex (shading) normal
+        const optix::int3 n_idx = nidx[rtGetPrimitiveIndex()];
+        const optix::float3 normal =
+            optix::normalize(nbuf[n_idx.y] * barycentrics.x + nbuf[n_idx.z] * barycentrics.y +
+                             nbuf[n_idx.x] * (1.0f - barycentrics.x - barycentrics.y));
 
-    // thePayload.radiance =
-    //     make_float3(barycentrics.x, barycentrics.y, 1.0f - barycentrics.x - barycentrics.y);
-    thePayload.radiance = shading_normal * 0.5f + 0.5f;
+        optix::float3 ref_dir = random_in_uhemisphere(thePayload.rand, normal);
+        optix::float3 hit_pos = theRay.origin + distance * theRay.direction;
+        optix::Ray bounce     = optix::make_Ray(hit_pos, ref_dir, 0, 0.001f, RT_DEFAULT_MAX);
+        rtTrace(sysRootObject, bounce, thePayload);
+        thePayload.radiance = varAttenuation * thePayload.radiance;
+    } else {
+        thePayload.radiance = optix::make_float3(0);
+    }
 }
 
 RT_PROGRAM void any_hit() {}
