d209c2a6418067675c8a54f339d25c9c06fea798
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-22 16:29:03 -0400

    Add translation header

--- src/rayterm/headers/unicode_translator.h
+++ src/rayterm/headers/unicode_translator.h
@@ -1,4 +1,8 @@
 #ifndef _RAYTERM_UNICODE_TRANSLATOR_H_
 #define _RAYTERM_UNICODE_TRANSLATOR_H_
 
+#include "raytrace"
+
+void translate_halfpixel(UnicodeBuffer* unicode, PixelBuffer* pixels);
+
 #endif
