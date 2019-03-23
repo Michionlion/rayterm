ab95c2d3f53330baa3d50d047c37cc38dfe9ad6e
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-23 04:24:10 -0400

    Update deps

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -22,7 +22,7 @@ export PATH="$GRADLE_LOCATION/bin:$PATH"
 wget http://www.leonerd.org.uk/code/libtermkey/libtermkey-0.22.tar.gz
 tar -xzvf libtermkey-0.22.tar.gz
 (
-\cd libtermkey-0.22
+\cd libtermkey-0.22 || exit
 make PREFIX=/usr all
 sudo make PREFIX=/usr install
 )
