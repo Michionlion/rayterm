4cd30e6e019206ae99337b44d956b88f4aaa0f00
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-23 04:07:17 -0400

    Add prefix in all instances of make

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -23,6 +23,6 @@ wget http://www.leonerd.org.uk/code/libtermkey/libtermkey-0.22.tar.gz
 tar -xzvf libtermkey-0.22.tar.gz
 (
 \cd libtermkey-0.22
-make all
+make PREFIX=/usr all
 sudo make PREFIX=/usr install
 )
