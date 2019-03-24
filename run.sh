7c54e085bbc0ff91c4b792a972e49eaba8cd075b
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-24 18:30:15 -0400

    Add more features and stuff to run.sh

--- run.sh
+++ run.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 FONT="DejaVuSansMono Nerd Font Mono"
-
+LOG="error.log"
 TYPE="$1"
 if [ -z "$TYPE" ]; then
     TYPE="debug"
 fi
 TYPE=${TYPE,,}
 
-test -f error.log && rm error.log
+: > $LOG
 
-if gradle installRtexplore${TYPE^}Executable --console=rich; then
+if gradle compileCu installRtexplore${TYPE^}Executable --console=rich; then
 
     printf "\nRunning %s executable...\n" $TYPE
     sleep 0.5
 
 if [[ -z "$NO_TERM" ]]; then
-    LD_LIBRARY_PATH=lib/libtickit/.libs:lib/cuda/lib64:lib/optix/lib64:build/libs/rayterm/shared/$TYPE TICKIT_DEBUG_FD=3 TICKIT_DEBUG_FLAGS=U xterm -T "i3-float" -geometry 100x50+200+150 -fa "$FONT" -fs 12 -e ./build/install/rtexplore/$TYPE/rtexplore 3>&1 2>error.log
+    LD_LIBRARY_PATH=lib/libtickit/.libs:lib/cuda/lib64:lib/optix/lib64:build/libs/rayterm/shared/$TYPE TICKIT_DEBUG_FD=4 TICKIT_DEBUG_FLAGS=U xterm -T "i3-float" -geometry 100x50+200+150 -fa "$FONT" -fs 12 -e ./build/install/rtexplore/$TYPE/rtexplore 4>&2
 else
-    LD_LIBRARY_PATH=lib/libtickit/.libs:lib/cuda/lib64:lib/optix/lib64:build/libs/rayterm/shared/$TYPE TICKIT_DEBUG_FD=3 TICKIT_DEBUG_FLAGS=U  ./build/install/rtexplore/$TYPE/rtexplore 3>&1 2>error.log
+    LD_LIBRARY_PATH=lib/libtickit/.libs:lib/cuda/lib64:lib/optix/lib64:build/libs/rayterm/shared/$TYPE TICKIT_DEBUG_FD=4 TICKIT_DEBUG_FLAGS=U  ./build/install/rtexplore/$TYPE/rtexplore 2> >(tee -a $LOG >&2)
 fi
-    LOG=$(cat error.log)
+    LOG_CONTENTS=$(cat $LOG)
 
-    if [ -z "$LOG" ]; then
+    if [ -z "$LOG_CONTENTS" ]; then
         printf "\n -- No Errors -- \n"
     else
         printf "\n -- Error Log -- \n"
-        echo "$LOG"
+        echo "$LOG_CONTENTS"
     fi
 fi
