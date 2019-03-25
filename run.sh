cb75ec2ce6fda71588b2d1160feef79df053434d
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-24 21:47:17 -0400

    Adjust terminal geometry and font size

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ if gradle compileCu installRtexplore${TYPE^}Executable --console=rich; then
     sleep 0.5
 
 if [[ -z "$NO_TERM" ]]; then
-    LD_LIBRARY_PATH=lib/libtickit/.libs:lib/cuda/lib64:lib/optix/lib64:build/libs/rayterm/shared/$TYPE COLORTERM=truecolor TICKIT_DEBUG_FD=4 TICKIT_DEBUG_FLAGS=U xterm -T "i3-float" -geometry 100x50+200+150 -fa "$FONT" -fs 12 -e ./build/install/rtexplore/$TYPE/rtexplore 4>&2
+    LD_LIBRARY_PATH=lib/libtickit/.libs:lib/cuda/lib64:lib/optix/lib64:build/libs/rayterm/shared/$TYPE COLORTERM=truecolor TICKIT_DEBUG_FD=4 TICKIT_DEBUG_FLAGS=U xterm -T "i3-float" -geometry 80x26+300+250 -fa "$FONT" -fs 9 -e ./build/install/rtexplore/$TYPE/rtexplore 4>&2
 else
     LD_LIBRARY_PATH=lib/libtickit/.libs:lib/cuda/lib64:lib/optix/lib64:build/libs/rayterm/shared/$TYPE COLORTERM=truecolor TICKIT_DEBUG_FD=4 TICKIT_DEBUG_FLAGS=U  ./build/install/rtexplore/$TYPE/rtexplore 2> >(tee -a $LOG >&2)
 fi
