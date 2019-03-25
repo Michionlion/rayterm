78bee15735efc1839d260e8174cc5d816c7ab547
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-24 21:28:16 -0400

    Remove old code from unicode_translator

--- src/rayterm/cpp/unicode_translator.cpp
+++ src/rayterm/cpp/unicode_translator.cpp
@@ -2,27 +2,20 @@
 #include <cstdlib>
 #include "unicode_buffer.h"
 
-// U+2580 (▀)
-#define UPPER_HALF_PIXEL_CHAR 0x2580
-
 UnicodeBuffer* translate_halfpixel(PixelBuffer* pixels) {
     return translate_halfpixel(pixels, new UnicodeBuffer(pixels->width, pixels->height / 2));
 }
 
 UnicodeBuffer* translate_halfpixel(PixelBuffer* pixels, UnicodeBuffer* unicode) {
-    pixels->map();
-
     int cell = 0;
     for (unsigned int y = 0; y < pixels->height; y += 2) {
         for (unsigned int x = 0; x < pixels->width; x++) {
-            auto fg = pixels->get(x, y);
-            auto bg = pixels->get(x, y + 1);
-            unicode->data[cell++] =
-                unicode_cell(UPPER_HALF_PIXEL_CHAR, fg.x, fg.y, fg.z, bg.x, bg.y, bg.z);
+            auto fg               = pixels->get(x, y);
+            auto bg               = pixels->get(x, y + 1);
+            unicode->data[cell++] = unicode_cell(fg.x, fg.y, fg.z, bg.x, bg.y, bg.z);
         }
     }
 
-    pixels->unmap();
 
     return unicode;
 }
